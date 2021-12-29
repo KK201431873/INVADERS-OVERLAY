ff643f6140f23290c91ed8af9342a529e1a2ceef
BuildTools <unconfigured@null.spigotmc.org>
2021-12-29 15:53:26 +0100

    moved ironblock.png to ressources folder added modid to build.gradle

--- src/main/java/KK201431873/invadersoverlaymod/InvadersOverlayMod.java
+++ src/main/java/KK201431873/invadersoverlaymod/InvadersOverlayMod.java
@@ -21,7 +21,6 @@ public class InvadersOverlayMod
     public static final String MODID = "invadersoverlaymod";
     public static final String VERSION = "1.0";
     public static final String NAME = "Hypixel Invaders Overlay Mod";
-    Minecraft mc = Minecraft.getMinecraft();
 
     @SidedProxy(clientSide = "KK201431873.invadersoverlaymod.proxy.ClientProxy", serverSide = "KK201431873.invadersoverlaymod.proxy.CommonProxy")
     public static CommonProxy proxy;
